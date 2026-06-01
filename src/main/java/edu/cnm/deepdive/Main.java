b42a0af1529311fa94987d575414af9e386893a9
nick-bennett <nbennett10@cnm.edu>
2026-06-01 16:29:26 -0600

    Reading Celsius from command line.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -3,9 +3,10 @@ package edu.cnm.deepdive;
 public class Main {
 
   void main(String[] args) {
-    double[] celsiusInputs = {0, 100, -40, 50};
+//    double[] celsiusInputs = {0, 100, -40, 50};
     Converter converter = new Converter();
-    for (double c : celsiusInputs) {
+    for (String arg : args) {
+      double c = Double.parseDouble(arg);
       double f = converter.convertC2F(c);
       System.out.printf("Celsius = %.2f; Fahrenheit = %.2f%n", c, f);
     }
