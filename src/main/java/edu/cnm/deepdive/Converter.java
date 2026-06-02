4ca4432913dfe7b45338ade77953a607bb630ff4
nick-bennett <nbennett10@cnm.edu>
2026-06-02 09:40:52 -0600

    Added Fahrenheit-to-Celsius; extracted constants.

--- src/main/java/edu/cnm/deepdive/Converter.java
+++ src/main/java/edu/cnm/deepdive/Converter.java
@@ -2,8 +2,15 @@ package edu.cnm.deepdive;
 
 public class Converter {
 
+  private static final double CELSIUS_TO_FAHRENHEIT_SCALE = 1.8;
+  private static final int FAHRENHEIT_OFFSET_FROM_CELSIUS = 32;
+
   public double convertC2F(double celsius) {
-    return 1.8 * celsius + 32;
+    return CELSIUS_TO_FAHRENHEIT_SCALE * celsius + FAHRENHEIT_OFFSET_FROM_CELSIUS;
+  }
+
+  public double convertF2C(double fahrenheit) {
+    return (fahrenheit - FAHRENHEIT_OFFSET_FROM_CELSIUS) / CELSIUS_TO_FAHRENHEIT_SCALE;
   }
 
 }
