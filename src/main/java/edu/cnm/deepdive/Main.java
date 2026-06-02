44a3a5dc86db2115d31099859683b918c5d340d1
nick-bennett <nbennett10@cnm.edu>
2026-06-02 14:53:15 -0600

    Replaced old-school switch with modern switch statement.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -24,13 +24,9 @@ public class Main {
         char modeFlag = Character.toUpperCase(input.strip().charAt(0));
 
         switch (modeFlag) {
-          case 'C':
-            mode = Mode.CELSIUS_TO_FAHRENHEIT;
-            break;
-          case 'F':
-            mode = Mode.FAHRENHEIT_TO_CELSIUS;
-            break;
-          default:
+          case 'C' -> mode = Mode.CELSIUS_TO_FAHRENHEIT;
+          case 'F' -> mode = Mode.FAHRENHEIT_TO_CELSIUS;
+          default -> System.err.println("Invalid input: " + input);
             // TODO: 6/2/26 Print error message to standard error.
         }
 
