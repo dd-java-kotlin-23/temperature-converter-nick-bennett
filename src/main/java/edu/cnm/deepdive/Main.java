272370289fc237a4311338795e7d720339c6aadf
nick-bennett <nbennett10@cnm.edu>
2026-06-02 14:21:49 -0600

    Implemented Scanner-based conversion.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -1,16 +1,43 @@
 package edu.cnm.deepdive;
 
+import java.util.Scanner;
+
 public class Main {
 
   void main(String[] args) {
-    // TODO: 6/2/26 Set initial conversion mode to Celsius-to-Fahrenheit.
-    // TODO: 6/2/26 Start reading from standard input.
+    // DONE: 6/2/26 Set initial conversion mode to Celsius-to-Fahrenheit.
+    Mode mode = Mode.CELSIUS_TO_FAHRENHEIT;
+    Converter converter = new Converter();
+    // DONE: 6/2/26 Start reading from standard input.
+    Scanner scanner = new Scanner(System.in);
     // TODO: 6/2/26 While there is any input remaining to process:
-    // TODO: 6/2/26   1. Get next input token.
-    //                 a. If token is a number, convert according to the current mode, and output result to standard output.
-    //                 b. If token is 'F', switch mode to Fahrenheit-to-Celsius for subsequent conversions.
-    //                 c. If token is 'C', switch mode to Celsius-to-Fahrenheit for subsequent conversions.
-    //                 d. If token is anything else, write error message to standard error.
+    while (scanner.hasNext()) {
+      if (scanner.hasNextDouble()) {
+        double input = scanner.nextDouble();
+        double output;
+        if (mode == Mode.CELSIUS_TO_FAHRENHEIT) {
+          output = converter.convertC2F(input);
+        } else {
+          output = converter.convertF2C(input);
+        }
+        // TODO: 6/2/26 Print conversion mode, input, output.
+        System.out.println("input = " + input + "; output = " + output);
+      } else {
+        String input = scanner.next();
+        char modeFlag = Character.toUpperCase(input.strip().charAt(0));
+        if (modeFlag == 'C') {
+          mode = Mode.CELSIUS_TO_FAHRENHEIT;
+        } else if (modeFlag == 'F') {
+          mode = Mode.FAHRENHEIT_TO_CELSIUS;
+        } else {
+          // TODO: 6/2/26 Print error message to standard error device.
+        }
+      }
+    }
+  }
+
+  private enum Mode {
+    CELSIUS_TO_FAHRENHEIT, FAHRENHEIT_TO_CELSIUS
   }
 
 }
